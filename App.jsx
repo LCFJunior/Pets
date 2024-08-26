21ad0f9280f405824b39cdc49f00679e3ce60ab8
Luiz Carlos Francisco Junior <lcfjunior04@gmail.com>
2024-08-25 23:39:20 -0300

    code 2

--- App.jsx
+++ App.jsx
@@ -1,27 +1,53 @@
 import React from 'react';
+import { StatusBar, View, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { StatusBar, View } from 'react-native';
 import Login from './pages/Login/Login';
 import ForgotPassword from './pages/ForgotPassword/ForgotPassword';
 import { Home } from './pages/Home/Home';
+import Notifications from './pages/Notifications/Notifications';
+import Pet from './pages/Pet/Pet';
+import Settings from './pages/Settings/Settings';
+
 const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <View>
-        <StatusBar barStyle={'light-content'} />
+      <View style={styles.container}>
+        <StatusBar 
+          translucent
+          backgroundColor="transparent"
+          barStyle="light-content"
+        />
+        <Stack.Navigator
+          initialRouteName="Login"
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: 'transparent',
+            },
+            headerShadowVisible: false,
+            headerTintColor: '#fff',
+            headerTransparent: true,
+            headerShown: true,
+          }}
+        >
+          <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
+          <Stack.Screen name="ForgotPassword" component={ForgotPassword} options={{ headerShown: false }} />
+          <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
+          <Stack.Screen name="Notifications" component={Notifications} options={{ headerShown: false }} />
+          <Stack.Screen name="Pet" component={Pet} options={{ headerShown: false }} />
+          <Stack.Screen name="Settings" component={Settings} options={{ headerShown: false }} />
+        </Stack.Navigator>
       </View>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={Login} options={{ headerShown: false }}/>
-        <Stack.Screen name="ForgotPassword" component={ForgotPassword} options={{ headerShown: false }}/>
-        <Stack.Screen name="Home" component={Home} options={{ headerShown: false }}/>
-      </Stack.Navigator>
     </NavigationContainer>
-)}
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default App;
-
-
-
